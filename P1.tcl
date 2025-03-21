9a43cec61aa725b11da2fb5df65fa28b6a6b50d5
Niharika Mahadevappa <niharikamahadevappa0@gmail.com>
2025-03-21 17:48:35 +0530

    read

---
+++ P1.tcl
@@ -0,0 +1,39 @@
+#Implement 3 nodes point to point network with duplex links between them. Set the queue size, vary the bandwidth and find the number of packets dropped.
+set ns [new Simulator]
+
+set tracefile [open out.tr w]
+$ns $trace-all $tracefile
+set namfile [open out.nam w]
+$ns $namtrace-all $namfile
+
+set n0[$ns node]
+set n1[$ns node]
+set n2[$ns node]
+
+$ns duplex-link $n0 $n1 10Mb 10ms DropTail
+$ns duplex-link $n1 $n2 10Mb 10ms DropTail
+
+$ns queue-limit $n0 $n1 5
+$ns queue-limit $n1 $n2 5
+
+set tcp [new Agent/TCP]
+$ns attach-agent $tcp
+set sink[new Agent/TCPSink]
+$ns attach-agent $sink
+
+set cbr [new Applicaation/Traffic/CBR]
+$ns attach-agent $cbr
+
+proc finish{} {
+  global ns tracefile namfile
+  $ns flush-trace
+  close $tracefile
+  close $namfile
+  exec nam out.nam &
+  exec echo "no of packets dropped" &
+  exec trace out.tr &
+  exit 0
+}
+$ns at 0.1 "colour blue"
+$ns at 0.5 "stop"
+$ns run
